988197c4016a70114e6e96abaa4ba3f82b9e6d75
Esteban Puentes <rapuentes@uan.edu.co>
2024-08-05 15:54:37 -0500

    adding internal repos

---
+++ front/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Inlaze movies",
+  description: "The best movies application available in the market, sign in and enjoy watching your favorites movies, series and more than that in our platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
