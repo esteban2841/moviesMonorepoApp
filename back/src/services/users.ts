98d375bafcbc21a1f03e11a7761bb96db604806a
Esteban Puentes <rapuentes@uan.edu.co>
2024-08-06 14:22:42 -0500

    added user retrieval to check if there is a user already logged in and return it, if not error handler added

--- back/src/services/users.ts
+++ back/src/services/users.ts
@@ -10,21 +10,25 @@ export class UsersService {
   async findAll(): Promise<User[]> {
     return await this.usersModel.find().exec();
   }
-  async addNewUser( userData: User ): Promise<User> {
-    const userExists = await this.usersModel.findOne({email: userData.email}).exec()
-    if( userExists ){
-        throw new Error('email is used and already exists')
+  async addNewUser(userData: User): Promise<User> {
+    const userExists = await this.usersModel
+      .findOne({ email: userData.email })
+      .exec();
+    if (userExists) {
+      throw new Error('email is used and already exists');
     }
     const newUser = new this.usersModel(userData);
     return newUser.save();
   }
-  async findOne( userData: any ): Promise<any> {
-    const userExists = await this.usersModel.findOne({email: userData.email}).exec()
-    if( !userExists ){
-        throw new Error('user is not registered')
+  async findOne(userData: any): Promise<any> {
+    const userExists = await this.usersModel
+      .findOne({ email: userData.email })
+      .exec();
+    if (!userExists) {
+      throw new Error('user is not registered');
     }
-    if( userExists ){
-        return userExists
+    if (userExists) {
+      return userExists;
     }
   }
 }
